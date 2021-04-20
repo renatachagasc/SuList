4bc62545a79f4f505ec0ee978efcb5fe78c069c6
Renata <rehcardosochagas@gmail.com>
2021-04-20 16:59:37 -0300

    Implementando o docker compose

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,10 @@
-const nodemailer = require('nodemailer');
+const express = require("express");
 
-let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-        user: "sulist.tarefas@gmail.com",
-        pass: "S#taref987"
-    },
-});
+const app = express();
 
-transporter.sendMail({
-    from: "Sulist <sulist.tarefas@gmail.com>",
-    to: "sulist.tarefas@gmail.com",
-    subject: "SuList ✔ Olá, vem conferir suas tarefas",
-    text: "Opa! Você não tem tarefas cadastradas",
-    html: "<b>Opa!</p> Você não tem tarefas cadastradas, vem com a gente cadastrar novas tarefas!!"
-}).then(message => {
-    console.log(message);
-}).catch(err => {
-    console.log(err);
-})
+
+app.get("/", (req, res) => {
+    res.send("Hi Docker!!!");
+  });
+
+app.listen(4200);
